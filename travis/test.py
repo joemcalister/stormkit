9cf7d4a70e9844d5eba5f1388a42c36702f46117
Joe McAlister <joe@lonestorm.com>
2017-03-14 19:50:58 +0000

    Converted decimal values to string before cast to decimal to fix a strange issue in all but python 2.7

--- travis/test.py
+++ travis/test.py
@@ -4,6 +4,7 @@ sen = MultiMoodSentimentAnalysis()
 result = sen.analyse_text("I hate Trump, I'm furious, but I love ice cream. Such a saddening, frightning, moment.")
 
 if result.error is None:
+    print result.decimals
     assert True
 else:
     assert False
