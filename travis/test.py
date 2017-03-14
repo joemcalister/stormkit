15acc60cbbf8fb8286efd5e42af9dab2fdbc4425
Joe McAlister <joe@lonestorm.com>
2017-03-14 21:11:14 +0000

    Updated test module

--- travis/test.py
+++ travis/test.py
@@ -1,7 +1,7 @@
 from stormkit.sentimentAnalysis import *
 
 sen = MultiMoodSentimentAnalysis()
-result = sen.analyse_text("I hate Trump, I'm furious, but I love ice cream. Such a saddening, frightning, moment.")
+result = sen.analyse_text("I hate Trump, I'm furious, but I love ice cream. Such a saddening moment. I'm frightened to be honest")
 
 if result.error is None:
     print(result.raw)
