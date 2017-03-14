872c3c71adeea4c0027aa9d54de572a5fcfed351
Joe McAlister <joe@lonestorm.com>
2017-03-14 19:55:20 +0000

    Fixing decimal issue with python 3.0 >

--- stormkit/sentimentAnalysis.py
+++ stormkit/sentimentAnalysis.py
@@ -35,7 +35,7 @@ class SentimentResult:
             if (total > 0):
                 ## double float cast fixes interesting type bug
                 ## round to two decimal places
-                self.decimals[result["emotion"]] = round(decimal.Decimal(str(float(float(result["occurences"])/total))),2)
+                self.decimals[result["emotion"]] = float(result["occurences"])/total)
             else:
                 self.decimals[result["emotion"]] = 0;
 
